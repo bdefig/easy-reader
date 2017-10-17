31f249b5a763f23de75ddf491fd8d1a457c7c204
Bryce DeFigueiredo <bdefig@gmail.com>
2017-10-16 23:51:14 -0600

    Filled in TextController functions

--- src/server/app.js
+++ src/server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const cookieParser = require('cookie-parser');
 
--- src/server/controllers/TextController.js
+++ src/server/controllers/TextController.js
@@ -2,7 +2,11 @@ const DocumentBlock = require('../models/DocumentBlock');
 const DocumentMetadata = require('../models/DocumentMetadata');
 
 exports.getAllDocumentMetadata = function (req, res, next) {
-
+    DocumentMetadata.find({})
+        .then(docs => {
+            res.send(docs);
+        })
+        .catch(err => console.log(err));
 }
 
 exports.getDocumentMetadataByID = function (req, res, next) {
@@ -27,24 +31,62 @@ exports.insertDocumentMetadata = function (req, res, next) {
 }
 
 exports.getOneDocumentBlock = function (req, res, next) {
-    
+    DocumentBlock.findOne({
+            documentID: req.params.documentID,
+            index: req.params.blockIndex
+        })
+        .then(block => {
+            res.json(block);
+        })
+        .catch(err => console.log(err));
 }
 
 exports.getDocumentBlocks = function (req, res, next) {
-    
+    DocumentBlock.find({
+            documentID: req.params.documentID,
+            index: { $gte : req.params.firstBlockIndex, $lte : req.params.lastBlockIndex }
+        })
+        .then(blocks => {
+            res.send(blocks)
+        })
+        .catch(err => console.log(err));
 }
 
 exports.insertDocumentBlocks = function (req, res, next) {
-    
+    let blockArray = [];
+    for (let block of req.body.blocks) {
+        const newDocBlock = new DocumentBlock ({
+            documentID: block.documentID,
+            index: block.index,
+            text: block.text,
+            textType: block.textType,
+            wordCount: block.wordCount
+        })
+        blockArray.push(newDocBlock);
+    }
+    let bulk = DocumentBlock.collection.initializeUnorderedBulkOp();
+    for (let block of blockArray) {
+        bulk.insert(block);
+    }
+    bulk.execute()
+        .then(onInsertedBlocks => res.json({Success: 'Document blocks inserted'}))
+        .catch(err => console.log(err));
 }
 
 exports.deleteDocumentByID = function (req, res, next) {
-
+    DocumentMetadata.findByIdAndRemove(req.params.documentID)
+        .then(deletedMetadata => {
+            DocumentBlock.deleteMany({
+                documentID: req.params.documentID
+            })
+        })
+        .then(deletedBlocks => res.json({Success: 'Document metadata and blocks removed'}))
+        .catch(err => console.log(err));
 }
 
 exports.clearAllDocuments = function (req, res, next) {
     DocumentMetadata.deleteMany({})
-        .then(res.json({Response: 'Document metadata cleared'}))
+        .then(res.json({Success: 'Document metadata cleared'}))
         .catch(err => console.log(err));
 }
 
