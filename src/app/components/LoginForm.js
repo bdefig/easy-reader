36101f4729fab01e6d40467fd6ff11f723366222
Bryce DeFigueiredo <bdefig@gmail.com>
2017-11-29 22:33:53 -0700

    Login and signup forms mostly working (up to submit)

--- src/app/components/LoginForm.js
+++ src/app/components/LoginForm.js
@@ -1,112 +1,257 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import './LoginForm.css';
 
-class LoginForm extends Component {
+export default class LoginForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: '',
-            password: ''
-        };
-    }
-
-    render() {
-        return (
-            <form className="Login-loginForm">
-                <div className="Login-loginInput">
-                    <input
-                        placeholder="Email"
-                        type="email"
-                        onChange={(text) => {
-                            this.setState({ email: text.target.value })
-                        }}
-                    />
-                </div>
-                <div className="Login-loginInput">
-                    <input
-                        placeholder="Password"
-                        type="password"
-                        onChange={(text) => {
-                            this.setState({ password: text.target.value })
-                        }}
-                    />
-                </div>
-                <input
-                    className="Login-submit"
-                    type="submit"
-                    value="Log In"
-                    onClick={() => {console.log(this.state)}}
-                />
-                <div className="Login-noAccount">
-                    Don't have an account? <a>Sign up</a>
-                </div>
-            </form>
-        );
-    }
-}
-
-class SignupForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+            signup: true,
+            displayErrors: false,
             name: '',
             email: '',
             password: ''
         };
+        this.submitSignup = this.submitSignup.bind(this);
+        this.submitLogin = this.submitLogin.bind(this);
+        this.toggleSignup = this.toggleSignup.bind(this);
+    }
+
+    submitSignup(event) {
+        event.preventDefault();
+        if (!event.target.checkValidity()) {
+            this.setState({
+                displayErrors: true
+            });
+            return;
+        }
+        console.log('Submitting Signup');
+        console.log('Name: ' + this.state.name + ', Email: ' +  this.state.email + ', Password: ' + this.state.password);
+    }
+
+    submitLogin(event) {
+        event.preventDefault();
+        if (!event.target.checkValidity()) {
+            this.setState({
+                displayErrors: true
+            });
+            return;
+        }
+        console.log('Submitting Login');
+        console.log('Email: ' +  this.state.email + ', Password: ' + this.state.password);
+    }
+
+    toggleSignup() {
+        this.setState((prevState) => {
+            return {
+                signup: !prevState.signup 
+            };
+        });
     }
 
     render() {
-        return (
-            <form className="Login-loginForm">
-                <div className="Login-loginInput">
-                    <input
-                        placeholder="Full Name"
-                        type="text"
-                        onChange={(text) => {
-                            this.setState({ name: text.target.value })
-                        }}
-                    />
+        if (this.state.signup) {
+            // Return Signup form
+            return (
+                <div className="Login-loginFormBox">
+                    <form
+                        className="Login-loginForm"
+                        onSubmit={this.submitSignup}
+                    >
+                        <div className="Login-loginInput">
+                            <input
+                                id="name"
+                                placeholder="Full Name"
+                                type="text"
+                                onChange={(text) => {
+                                    this.setState({ name: text.target.value })
+                                }}
+                            />
+                        </div>
+                        <div className="Login-loginInput">
+                            <input
+                                id="email"
+                                placeholder="Email"
+                                type="email"
+                                onChange={(text) => {
+                                    this.setState({ email: text.target.value })
+                                }}
+                            />
+                        </div>
+                        <div className="Login-loginInput">
+                            <input
+                                id="password"
+                                placeholder="Password"
+                                type="password"
+                                onChange={(text) => {
+                                    this.setState({ password: text.target.value })
+                                }}
+                            />
+                        </div>
+                        <input
+                            className="Login-submit"
+                            type="submit"
+                            value="Sign Up"
+                            onClick={() => {console.log(this.state)}}
+                        />
+                    </form>
+                    <div className="Login-noAccount">
+                        Already have an account? <a onClick={this.toggleSignup}>Log in</a>
+                    </div>
                 </div>
-                <div className="Login-loginInput">
-                    <input
-                        placeholder="Email"
-                        type="email"
-                        onChange={(text) => {
-                            this.setState({ email: text.target.value })
-                        }}
-                    />
+            );
+        } else {
+            // Return Login form
+            return (
+                <div className="Login-loginFormBox">
+                    <form
+                        className="Login-loginForm"
+                        onSubmit={this.submitLogin}
+                    >
+                        <div className="Login-loginInput">
+                            <input
+                                id="email"
+                                placeholder="Email"
+                                type="email"
+                                onChange={(text) => {
+                                    this.setState({ email: text.target.value })
+                                }}
+                            />
+                        </div>
+                        <div className="Login-loginInput">
+                            <input
+                                id="password"
+                                placeholder="Password"
+                                type="password"
+                                onChange={(text) => {
+                                    this.setState({ password: text.target.value })
+                                }}
+                            />
+                        </div>
+                        <input
+                            className="Login-submit"
+                            type="submit"
+                            value="Log In"
+                            onClick={() => {console.log(this.state)}}
+                        />
+                    </form>
+                    <div className="Login-noAccount">
+                        Don't have an account? <a onClick={this.toggleSignup}>Sign up</a>
+                    </div>
                 </div>
-                <div className="Login-loginInput">
-                    <input
-                        placeholder="Password"
-                        type="password"
-                        onChange={(text) => {
-                            this.setState({ password: text.target.value })
-                        }}
-                    />
-                </div>
-                <input
-                    className="Login-submit"
-                    type="submit"
-                    value="Sign Up"
-                    onClick={() => {console.log(this.state)}}
-                />
-                <div className="Login-noAccount">
-                    Already have an account? <a>Log in</a>
-                </div>
-            </form>
-        );
+            );
+        }
     }
 }
 
-export { LoginForm, SignupForm };
+// class LoginForm extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             email: '',
+//             password: ''
+//         };
+//         this.switchToSignup = this.switchToSignup.bind(this);
+//     }
 
-// const LoginInput = ({ placeholder }) => {
-//     return (
-//         <div className="Login-loginInput">
-//             <input placeholder={placeholder} />
-//         </div>
-//     );
+//     switchToSignup() {
+//         alert('Switching to signup');
+//         this.props.switchToSignup();
+//     }
+
+//     render() {
+//         return (
+//             <form className="Login-loginForm">
+//                 <div className="Login-loginInput">
+//                     <input
+//                         placeholder="Email"
+//                         type="email"
+//                         onChange={(text) => {
+//                             this.setState({ email: text.target.value })
+//                         }}
+//                     />
+//                 </div>
+//                 <div className="Login-loginInput">
+//                     <input
+//                         placeholder="Password"
+//                         type="password"
+//                         onChange={(text) => {
+//                             this.setState({ password: text.target.value })
+//                         }}
+//                     />
+//                 </div>
+//                 <input
+//                     className="Login-submit"
+//                     type="submit"
+//                     value="Log In"
+//                     onClick={() => {console.log(this.state)}}
+//                 />
+//                 <div className="Login-noAccount">
+//                     Don't have an account? <button onClick={this.switchToSignup}>Sign up</button>
+//                 </div>
+//             </form>
+//         );
+//     }
 // }
 
-// export default LoginForm;
+// class SignupForm extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             name: '',
+//             email: '',
+//             password: ''
+//         };
+//         this.switchToLogin = this.switchToLogin.bind(this);
+//     }
+
+//     switchToLogin() {
+//         alert('Switching to login');
+//         this.props.switchToLogin();
+//     }
+
+//     render() {
+//         return (
+//             <form className="Login-loginForm">
+//                 <div className="Login-loginInput">
+//                     <input
+//                         placeholder="Full Name"
+//                         type="text"
+//                         onChange={(text) => {
+//                             this.setState({ name: text.target.value })
+//                         }}
+//                     />
+//                 </div>
+//                 <div className="Login-loginInput">
+//                     <input
+//                         placeholder="Email"
+//                         type="email"
+//                         onChange={(text) => {
+//                             this.setState({ email: text.target.value })
+//                         }}
+//                     />
+//                 </div>
+//                 <div className="Login-loginInput">
+//                     <input
+//                         placeholder="Password"
+//                         type="password"
+//                         onChange={(text) => {
+//                             this.setState({ password: text.target.value })
+//                         }}
+//                     />
+//                 </div>
+//                 <input
+//                     className="Login-submit"
+//                     type="submit"
+//                     value="Sign Up"
+//                     onClick={() => {console.log(this.state)}}
+//                 />
+//                 <div className="Login-noAccount">
+//                     Already have an account? <button onClick={this.switchToLogin}>Log in</button>
+//                 </div>
+//             </form>
+//         );
+//     }
+// }
+
+// export { LoginForm, SignupForm };
--- src/app/containers/Login.js
+++ src/app/containers/Login.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
+import { 
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from 'react-router-dom';
 import './Login.css';
-import { LoginForm, SignupForm } from '../components/LoginForm';
+import LoginForm from '../components/LoginForm';
+
 
 export default class Login extends Component {
     render() {
-        return(
+        return (
             <div className="Login-container">
                 <LoginForm />
-                <SignupForm />
             </div>
         );
     }
 }
+
+// export default class Login extends Component {
+//     constructor(props) {
+//         alert('New Login component!');
+//         super(props);
+//         this.state = {
+//             signUp: true
+//         };
+//         this.switchToLogin = this.switchToLogin.bind(this);
+//         this.switchToSignup = this.switchToSignup.bind(this);
+//     }
+
+//     switchToLogin() {
+//         this.setState({ signup: false });
+//     }
+
+//     switchToSignup() {
+//         this.setState({ signup: true });
+//     }
+
+//     render() {
+//         return (
+//             <div className="Login-container">
+//                 {this.state.signUp ? 
+//                     <SignupForm switchToLogin={this.switchToLogin} /> :
+//                     <LoginForm switchToSignup={this.switchToSignup} />
+//                 }
+//             </div>
+//         );
+//     }
+// }
