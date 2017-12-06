ba8e28ee068a8a732cfc8879023e1811e99871fa
Bryce DeFigueiredo <bdefig@gmail.com>
2017-12-05 22:20:19 -0700

    Added password check helper function

--- src/server/helpers/AuthenticationHelper.js
+++ src/server/helpers/AuthenticationHelper.js
@@ -2,5 +2,11 @@ const bcrypt = require('bcrypt');
 const saltRounds = 12;
 
 exports.hashPassword = function (password) {
+    // Returns a promise with the result being the hash
     return bcrypt.hash(password, saltRounds);
+}
+
+exports.checkPassword = function (passwordToCheck, actualHash) {
+    // Returns a promise with the result being true (success) or false (failure)
+    return bcrypt.compare(passwordToCheck, actualHash);
 }
