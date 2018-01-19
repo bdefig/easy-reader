e205e8ef32358ae1cf285201b7bf0593217cebb1
Bryce DeFigueiredo <bdefig@gmail.com>
2018-01-18 22:42:56 -0700

    Removed old user accounts schema

--- src/server/models/UserDocumentProgress.js
+++ src/server/models/UserDocumentProgress.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserDocumentProgressSchema = Schema({
-    user: {type: Schema.ObjectId, ref: 'UserAccount', required: true},
+    user: {type: Schema.ObjectId, ref: 'User', required: true},
     document: {type: Schema.ObjectId, ref: 'DocumentMetadata', required: true},
     currentBlock: {type: Number, default: 0},
     lastAccessed: {type: Date, default: Date.now()}
